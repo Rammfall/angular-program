a49530a157635a15c67669dc7b3a69fe70bfebcd
rammfall <15bogdan17@gmail.com>
2022-05-11 12:39:43 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoursesComponent } from './pages/courses/courses/courses.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LogoComponent } from './shared/logo/logo.component';
 
 @NgModule({
-  declarations: [CoursesComponent],
+  declarations: [
+    CoursesComponent,
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/courses/courses.module.ts
+++ src/app/pages/courses/courses.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses/courses.component';
-import { AppComponent } from '../../app.component';
-import { HeaderComponent } from '../../shared/header/header.component';
-import { FooterComponent } from '../../shared/footer/footer.component';
-import { LogoComponent } from '../../shared/logo/logo.component';
 
 @NgModule({
-  declarations: [
-    CoursesComponent,
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    LogoComponent,
-  ],
+  declarations: [CoursesComponent],
   imports: [CommonModule],
 })
 export class CoursesModule {}
